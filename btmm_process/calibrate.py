7f7e60a05bf99e7e83898f0aa1422e5cf6c43dbf
Karl Lapo <karl.lapo@uni-bayreuth.de>
2018-06-19 17:28:08 +0200

    Updated to speedier version of calibration.

--- btmm_process/calibrate.py
+++ btmm_process/calibrate.py
@@ -32,71 +32,82 @@ def prepCalibrate(cfg):
     return ds
 
 
-def matrixInversion(ds, refConfig):
-    '''
-    Solve for the calibration parameters
-    '''
-    # Unpack the configuration file
-    refField1 = refConfig['refField1']
-    refField3 = refConfig['refField2']
-    refField2 = refConfig['refField3']
-    refLoc1 = refConfig['refLoc1']
-    refLoc2 = refConfig['refLoc2']
-    refLoc3 = refConfig['refLoc3']
+def matrixInversion(dsCal, refField1, refField2, refField3,
+                    refLoc1, refLoc2, refLoc3, direction='forward'):
+    refT1 = dsCal[refField1] + 273.15
+    refT2 = dsCal[refField2] + 273.15
+    refT3 = dsCal[refField3] + 273.15
 
-    # Reference temperatures
-    refT1 = ds[refField1] + 273.15
-    refT2 = ds[refField2] + 273.15
-    refT3 = ds[refField3] + 273.15
+    # For double ended calibration, the reverse pulse needs to have LAF flipped
+    if direction == 'reverse':
+        dsCal['LAF'] = np.flip(dsCal.LAF.values, 0)
 
-    section1 = ds.swap_dims({'LAF': 'location'}).sel(location=refLoc1)
-    section2 = ds.swap_dims({'LAF': 'location'}).sel(location=refLoc2)
-    section3 = ds.swap_dims({'LAF': 'location'}).sel(location=refLoc3)
+    section1 = dsCal.swap_dims({'LAF': 'location'}).sel(location=refLoc1)
+    section2 = dsCal.swap_dims({'LAF': 'location'}).sel(location=refLoc2)
+    section3 = dsCal.swap_dims({'LAF': 'location'}).sel(location=refLoc3)
 
     ref_z1 = section1.LAF.mean(dim='location')
     ref_z2 = section2.LAF.mean(dim='location')
-    ref_z3 = section3.LAF.mean(dim='location')
+    ref_z3 = section2.LAF.mean(dim='location')
 
     # Amplitudes of stokes/anti-stokes
-    stokesRatio1 = np.log(section1.Ps / section1.Pas).mean(dim='location')
-    stokesRatio2 = np.log(section2.Ps / section2.Pas).mean(dim='location')
-    stokesRatio3 = np.log(section3.Ps / section3.Pas).mean(dim='location')
+    if 'logPsPas' in dsCal:
+        stokesRatio1 = section1.logPsPas.mean(dim='location')
+        stokesRatio2 = section2.logPsPas.mean(dim='location')
+        stokesRatio3 = section3.logPsPas.mean(dim='location')
+    else:
+        stokesRatio1 = np.log(section1.Ps / section1.Pas).mean(dim='location')
+        stokesRatio2 = np.log(section2.Ps / section2.Pas).mean(dim='location')
+        stokesRatio3 = np.log(section3.Ps / section3.Pas).mean(dim='location')
 
     # Allocate the calibration variables and manual temperature
-    gamma = np.ones(np.shape(ds.time.values)) * -9999.
-    C = np.ones(np.shape(ds.time.values)) * -9999.
-    delta_alpha = np.ones(np.shape(ds.time.values)) * -9999.
-    manualTemp = np.ones(np.shape(ds.temp.values)) * -9999.
+    gamma = np.ones(np.shape(dsCal.time.values)) * -9999.
+    C = np.ones(np.shape(dsCal.time.values)) * -9999.
+    delta_alpha = np.ones(np.shape(dsCal.time.values)) * -9999.
+    manualTemp = np.ones(np.shape(dsCal.temp.values)) * -9999.
 
     # Within each time step solve for the calibration parameters
-    for n, t in enumerate(ds.time):
-        print('Time step ' + str(n) + ' of ' + str(np.size(ds.time.values)), end="\r", flush=True)
-
+    for n, t in enumerate(dsCal.time):
+        print('Time step ' + str(n + 1) + ' of '
+              + str(np.size(dsCal.time.values)), end="\r", flush=True)
+        T1 = refT1.sel(time=t).values
+        T2 = refT2.sel(time=t).values
+        T3 = refT3.sel(time=t).values
+        sR1 = stokesRatio1.sel(time=t).values
+        sR2 = stokesRatio2.sel(time=t).values
+        sR3 = stokesRatio3.sel(time=t).values
 
         # A matrix
-        A = [[1, -refT1.sel(time=t), refT1.sel(time=t) * ref_z1],
-             [1, -refT2.sel(time=t), refT2.sel(time=t) * ref_z2],
-             [1, -refT3.sel(time=t), refT3.sel(time=t) * ref_z3],
-            ]
+        A = [[1, -T1, T1 * ref_z1],
+             [1, -T2, T2 * ref_z2],
+             [1, -T3, T3 * ref_z3],
+             ]
 
         # b matrix
-        b = [[refT1.sel(time=t) * stokesRatio1.sel(time=t)],
-             [refT2.sel(time=t) * stokesRatio2.sel(time=t)],
-             [refT3.sel(time=t) * stokesRatio3.sel(time=t)],
-            ]
+        b = [[T1 * sR1],
+             [T2 * sR2],
+             [T3 * sR3],
+             ]
 
         x = np.linalg.solve(A, b)
 
+        # Store the calibration coefficients
         gamma[n] = x[0]
         C[n] = x[1]
         delta_alpha[n] = x[2]
 
-        #############################
-        ## Recalculate temperature ##
-        #############################
-        manualTemp[n] = gamma[n] / (np.log(ds.Ps.sel(time=t) / ds.Pas.sel(time=t)) + C[n] - delta_alpha[n] * ds.LAF)
+        # Recalculate temperature for this time step
+        manualTemp[n] = gamma[n] / (np.log(dsCal.Ps.sel(time=t)
+                                    / dsCal.Pas.sel(time=t))
+                                    + C[n] - delta_alpha[n] * dsCal.LAF)
+
+    # For double ended calibration, the reverse pulse needs to have LAF flipped
+    if direction == 'reverse':
+        dsCal['LAF'] = np.flip(dsCal.LAF.values, 0)
 
     # Assign calibrated temperature to dataset
-    ds['manualTemp'] = (('time', 'LAF'), manualTemp - 273.15)
+    dsCal['manualTemp'] = (('time', 'LAF'), manualTemp - 273.15)
+    print('')
+    print('Calibration done...')
 
-    return(ds)
+    return(dsCal, gamma, C, delta_alpha)
