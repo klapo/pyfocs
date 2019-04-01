ae7029e0b48c838800fb3f38c571f438606cbaad
Karl Lapo <karl.lapo@uni-bayreuth.de>
2019-04-01 17:19:40 +0200

    PyFOX QoL update, cont. - further checks for the existence of certain fields/variables - no longer returns the `calibrated_temp` field when it is not requested or possible to make. Warnings updated - further changes to handle segmented vs continuous data streams

--- PyFOX.py
+++ PyFOX.py
@@ -29,17 +29,7 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.simplefilter(action='ignore', category=RuntimeWarning)
 
-#%% open the config file
-root = tk.Tk()
-# to close this stupid root window that doesn't let itself be closed anymore AT ALL otherwise
-root.withdraw()
-# Allowed file types for the configuration file. Any file type is allowed as
-# long as it is a yaml .yml file.
-ftypes = [
-    ('yaml configuration file', '*.yml'),
-]
-filename_configfile = filedialog.askopenfilename(filetypes=ftypes)
-# filename_configfile = '/Users/karllapo/Desktop/proj/DarkMix_LEOPOLD/example_data/LEOPOLD_initial_config.yml'
+filename_configfile = '/Users/karllapo/Desktop/proj/DarkMix_SOPHABS/data/SOPHABS_config.yml'
 
 with open(filename_configfile, 'r') as stream:
     config_user = yaml.load(stream)
@@ -66,8 +56,11 @@ dir_processed = os.path.join(config_user['directories']['dir_pre'],
 if not os.path.exists(dir_processed):
     os.makedirs(dir_processed)
 
-# find all experiments to be processed and make a list of them
-os.chdir(dir_original)
+# find all experiments to be processed/calibrated and make a list of them
+try:
+    os.chdir(dir_original)
+except FileNotFoundError:
+    os.chdir(dir_processed)
 contents = os.listdir()
 DTS_folders = [c for c in contents if 'external' not in c and not c[0] == '.']
 
@@ -82,15 +75,15 @@ for c in external_data_content:
         continue
     # Look for key phrases used in indicating an external data file.
     # Check for the RBR solo PT100s
-    if 'RBR' in c and not c[0] == '.':
+    if 'RBR' in c.casefold() and not c[0] == '.':
         external_data_folders['RBR'] = [os.path.join(dir_ext, c, tf)
                                         for tf in temp_files
                                         if 'RBR' in tf and not c[0] == '.']
     # Check for a datalogger or multiplexer TOA5 file.
-    if ('Logger' in c or 'ts_data' in c) and not c[0] == '.' and '.dat' in c:
+    if ('logger' in c.casefold() or 'ts_data' in c.casefold()) and not c[0] == '.':
         external_data_folders['logger'] = [os.path.join(dir_ext, c, tf)
                                            for tf in temp_files
-                                           if not c[0] == '.']
+                                           if '.dat' in tf]
 
 # Loop through all of the DTS data directories
 # assemble internal config for each dts folder within the experiment folder
@@ -108,7 +101,7 @@ for dtsf in DTS_folders:
     internal_config[dtsf]['archive']['targetPath'] = os.path.join(dir_processed, dtsf)
     internal_config[dtsf]['directories']['dirData'] = os.path.join(dir_processed, dtsf)
     internal_config[dtsf]['directories']['dirProcessed'] = os.path.join(dir_processed, dtsf)
-    internal_config[dtsf]['fileName']['filePrefix'] = config_user['directories']['folder_raw_data'] + '_' + dtsf
+    internal_config[dtsf]['directories']['fileName']['prefix'] = config_user['directories']['folder_raw_data'] + '_' + dtsf
     # create the folder for the processed files if it doesn't already exist
     if not os.path.exists(internal_config[dtsf]['archive']['targetPath']):
         os.makedirs(internal_config[dtsf]['archive']['targetPath'])
@@ -168,30 +161,37 @@ if config_user['flags']['ref_temp_flag'] == 'external':
         logger_col_names = pd.read_csv(temp_extdata[0], header=1,
                                        nrows=0, index_col=0)
         # Empty dictionary for the pandas Dataframe objects.
-        pt100s = {}
+        pt100s = []
 
         # Loop over every TOA5 file found, load, format, resample, and concat
         for dat in temp_extdata:
             # Deal with PT100s on a multiplexer
             if extdat_source == 'logger':
-                new_data = pd.read_csv(logdat, header=None, skiprows=4,
+                new_data = pd.read_csv(dat, header=None, skiprows=4,
                                        index_col=0, parse_dates=True,
                                        infer_datetime_format=True, sep=',')
                 new_data.columns = logger_col_names.columns
             # Deal with any RBRs
             elif extdat_source == 'RBR':
-                new_data = pd.read_csv(solo, header=0,
+                new_data = pd.read_csv(dat, header=0,
                                        index_col=0, parse_dates=True,
                                        infer_datetime_format=True, sep=',')
-                new_data.columns = [os.path.split(solo)[1].split('.')[0]]
+                new_data.columns = [os.path.split(dat)[1].split('.')[0]]
 
-            # Resample to the desired frequency.
+#             # Resample to the desired frequency.
             new_data.index.names = ['time']
-            new_data = new_data.resample(config_user['dataProperties']['resampling_time']).mean()
             # Add the external data stream to a dictionary.
-            pt100s[dat] = new_data
+            pt100s.append(new_data)
             # Concatenate all of the external data files into a DataFrame
-        pt100s = pd.concat([pt100s], axis='columns')
+
+        if extdat_source == 'RBR':
+            # Put together the RBR's into a single Dataframe
+            pt100s = pd.concat(pt100s, axis='columns')
+        elif extdat_source == 'logger':
+            # Concatenate along the time dimension for the logger data.
+            pt100s = pd.concat(pt100s, axis=0)
+
+        # Need to force a monotically increasing time series here.
 
         # This check is specific to the DarkMix wind tunnel experiments.
         if hasattr(pt100s, 'experiment_flag_Max'):
@@ -231,24 +231,45 @@ if config_user['flags']['ref_temp_flag'] == 'external':
         # (most other observations).
         else:
             experiment_flag = False
-            temp_pts = pt100s
-            drop_columns = [cols for cols in temp_pts.columns
-                            if cols not in probe1Cols
-                            and cols not in probe2Cols
-                            and cols not in other_cols]
-            temp_pts = temp_pts.drop(drop_columns, axis=1)
-
             # Conver the datetime to UTC
-            time = temp_pts.index.tz_localize(tz).tz_convert('UTC')
+            time = pt100s.index.tz_localize(tz).tz_convert('UTC')
 
             # Create a warm and cold bath average
-            probe1 = xr.DataArray.from_series(temp_pts[probe1Cols].mean(axis=1))
-            probe2 = xr.DataArray.from_series(temp_pts[probe2Cols].mean(axis=1))
-            others = xr.Dataset.from_dataframe(temp_pts[other_cols])
-            external_data = xr.Dataset({probe1Name: ('time', probe1),
-                                        probe2Name: ('time', probe2)},
-                                        coords={'time': time.values})
-            external_data = xr.concat([others, external_data])
+            if ((probe1Cols and probe1Cols in pt100s) and (probe2Cols and probe2Cols in pt100s)):
+                probe1 = xr.DataArray.from_series(pt100s[probe1Cols].mean(axis=1))
+                probe2 = xr.DataArray.from_series(pt100s[probe2Cols].mean(axis=1))
+                external_data = xr.Dataset({probe1Name: ('time', probe1),
+                                            probe2Name: ('time', probe2)},
+                                           coords={'time': time.values})
+            else:
+                # No reference PT100 columns were found
+                external_data = None
+
+            if any(set(other_cols).intersection(set(pt100s))):
+                # Search for the other data columns specified by the user
+                others = []
+                for oc in other_cols:
+                    others.append(xr.DataArray.from_series(pt100s[oc]))
+                others = xr.merge(others)
+
+                # Merge the reference and other datastreams
+                if external_data:
+                    external_data = xr.concat([others,
+                                               external_data],
+                                              dim='time')
+                else:
+                    external_data = others
+
+            # Sort by date to create a monotonic Dataset
+            external_data = external_data.sortby(external_data.time)
+            # Create a regular interval time stamp index
+            reg_time_pd = pd.date_range(start=pd.to_datetime(external_data.time.values[0]),
+                                        end=pd.to_datetime(external_data.time.values[0]),
+                                        freq=delta_t)
+            external_data = external_data.interp(time=reg_time_pd,
+                                                 kwargs={'fill_value': 'extrapolate'})
+            external_data = external_data.resample(config_user['dataProperties']['resampling_time']).mean()
+
 else:
     external_data_flag = False
 
@@ -256,8 +277,14 @@ else:
 # Create an empty dictionary to hold the DTS data
 allExperiments = {}
 labels_general = config_user['loc_general']
-labels_ms = config_user['loc_ms']
-labels_ref_instr = config_user['loc_ref_instr']
+if 'loc_ms' in config_user:
+    labels_ms = config_user['loc_ms']
+else:
+    labels_ms = None
+if 'loc_ref_instr' in config_user:
+    labels_ref_instr = config_user['loc_ref_instr']
+else:
+    labels_ref_instr = None
 
 print('-------------')
 print('Processing and Calibrating DTS temperature data.')
@@ -286,7 +313,7 @@ for dtsf in dir_data:
 
     for nraw, raw_nc in enumerate(ncfiles):
         # Name of the output file for this archive chunk
-        outname_suffix = config_user['fileName']['fileSuffix']
+        outname_suffix = config_user['directories']['fileName']['suffix']
         outname_date = '_'.join(raw_nc.split('.')[0].split('_')[1:])
         outname = dtsf + '_processed_' + outname_date + outname_suffix + '.nc'
         print('Processing ' + outname_date + ' (' + str(nraw + 1)
@@ -297,10 +324,14 @@ for dtsf in dir_data:
 
         # Assigning to the experiments dictionary
         dstemp = btmm_process.labelLoc_general(dstemp, labels_general)
-        dstemp = btmm_process.labelLoc_additional(dstemp, labels_ms, 'loc_ms')
-        dstemp = btmm_process.labelLoc_additional(dstemp,
-                                                  labels_ref_instr,
-                                                  'loc_ref_instr')
+        if labels_ms:
+            dstemp = btmm_process.labelLoc_additional(dstemp,
+                                                      labels_ms,
+                                                      'loc_ms')
+        if labels_ref_instr:
+            dstemp = btmm_process.labelLoc_additional(dstemp,
+                                                      labels_ref_instr,
+                                                      'loc_ref_instr')
 
         # Drop the unnecessary negative LAF indices
         dstemp = dstemp.sel(LAF=dstemp['LAF'] > 0)
@@ -338,32 +369,31 @@ for dtsf in dir_data:
 
 #%% Construct dataset with all experiments/over all the measurement duration
         if external_data_flag:
-            # Let's get a deep copy of the reference data to work with.
-            ext_dat = copy.deepcopy(external_data[dtsf])
+            if experiment_flag:
+                # Let's get a deep copy of the reference data to work with.
+                ext_dat = copy.deepcopy(external_data[dtsf])
+            else:
+                ext_dat = copy.deepcopy(external_data)
             # Reindex the reference data to our chunk of DTS data.
             ext_dat = ext_dat.reindex_like(dstemp.time,
                                            method='nearest',
                                            tolerance=1)
 
-            # Assign the reference pt100s to the xarray Dataset
-            dstemp[probe1Name] = ext_dat[probe1Name]
-            dstemp[probe2Name] = ext_dat[probe2Name]
+            # Assign the external data to the DTS Dataset
+            for ed in external_data:
+                dstemp[ed] = ext_dat[ed]
 
-        # Calibrate the temperatures. If the bath pt100s and dts do not line up
+       # Calibrate the temperatures. If the bath pt100s and dts do not line up
         # in time, do not calibrate.
         if (np.size(np.flatnonzero(~np.isnan(dstemp.temp.values))) > 0) and (config_user['flags']['calibration_flag']):
             dstemp, _, _, _ = btmm_process.matrixInversion(dstemp, internal_config[dtsf])
         elif not config_user['flags']['calibration_flag']:
-            # Just return some nans here and remind the user.
-            dstemp['cal_temp'] = (['time', 'LAF'],
-                                  np.ones_like(dstemp.temp.values) * np.nan)
-            print('No calibration required, returning NaN cal_temp array.')
+            # Just remind the user.
+            print('No calibration required, the cal_temp field will not be returned.')
         else:
-            # Just return some nans here and notify the user.
-            dstemp['cal_temp'] = (['time', 'LAF'],
-                                  np.ones_like(dstemp.temp.values) * np.nan)
+            # Notify the user.
             print('PT100 and DTS data do not line up in time for ' + dtsf)
-            print('The cal_temp field will contain NaNs.')
+            print('The cal_temp field will not be returned.')
 
         # Rename the instrument reported temperature field
         dstemp.rename({'temp': 'instr_temp'}, inplace=True)
@@ -371,6 +401,7 @@ for dtsf in dir_data:
         # Output the calibrated data
         # if not os.path.exists(outname):
         dstemp.to_netcdf(outname, engine='netcdf4')
+        print(dstemp)
         # else:
             # print('A netCDF with the name ' + dtsf + '_processed.nc already exists. The file was not overwritten.')
 
@@ -379,9 +410,12 @@ for dtsf in dir_data:
 # Write a csv of the locations because Matlab has problems reading them from the netcdf
 header = ['section_name', 'beginning (LAF)', 'end (LAF)']
 
-if ((config_user['loc_general'] is not None) and
-        (config_user['loc_ms'] is not None) and
-        (config_user['loc_ref_instr'] is not None)):
+if (('loc_general' in config_user) and
+    ('loc_ms' in config_user) and
+    ('loc_ref_instr') and
+    (config_user['loc_general'] is not None) and
+    (config_user['loc_ms'] is not None) and
+    (config_user['loc_ref_instr'] is not None)):
     filename_locations = os.path.join(dir_processed, dtsf, dtsf + '_locations.csv')
     locations_file = open(filename_locations, 'w')
     with locations_file:
