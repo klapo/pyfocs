6f065ad8b49a4890008ebd4da5f667d5768ec7f7
Anita <anita.freundorfer@uni-bayreuth.de>
2019-09-16 15:01:23 +0200

    resampling reference temperature data to make sure the reindexing to a common time stamp works

--- PyFOX.py
+++ PyFOX.py
@@ -242,7 +242,9 @@ except KeyError:
 if ('step_loss_LAF' in config_user['dataProperties']
         and 'step_loss_correction' in config_user['dataProperties']):
     step_loss_flag = True
-
+else: 
+    step_loss_flag = False
+    
 # Determine write mode:
 try:
     write_mode = internal_config[exp_name]['flags']['write_mode']
@@ -291,7 +293,8 @@ for exp_name in experiment_names:
             # Get the metdata
             os.chdir(dir_ext)
             ref_data = xr.open_dataset(config_user['directories']['filename_external'])
-
+            ref_data = ref_data.resample(time = config_user['dataProperties']['resampling_time']).interpolate('linear')
+                
             probe1_name = internal_config[exp_name]['dataProperties']['probe1Temperature']
             probe2_name = internal_config[exp_name]['dataProperties']['probe2Temperature']
 
@@ -385,17 +388,17 @@ for exp_name in experiment_names:
 
             # Add in external reference data here
             if internal_config[exp_name]['flags']['ref_temp_option'] == 'external':
-                ref_data.reindex_like(dstemp.time,
+                temp_ref_data = ref_data.reindex_like(dstemp.time,
                                       method='nearest',
                                       tolerance=1)
-
-                dstemp[probe1_name] = ref_data[probe1_name]
-                dstemp[probe2_name] = ref_data[probe2_name]
+                
+                dstemp[probe1_name] = temp_ref_data[probe1_name]
+                dstemp[probe2_name] = temp_ref_data[probe2_name]
 
                 # Add additional external data for this data stream.
                 if internal_config[exp_name]['dataProperties']['external_fields']:
                     for ext_dat in internal_config[exp_name]['dataProperties']['external_fields']:
-                        dstemp[ext_dat] = ref_data[ext_dat]
+                        dstemp[ext_dat] = temp_ref_data[ext_dat]
 
                 # If the bath pt100s and dts do not line up in time,
                 # notify the user.
