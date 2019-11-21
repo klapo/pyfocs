efbc7d87b05a7c869a67aec6618e3a3ff7d467d3
Karl Lapo <karl.lapo@uni-bayreuth.de>
2019-11-21 11:24:34 +0100

    New features:
    - uniform z-grid for a time varying balloon height.

--- btmm_process/flyfox.py
+++ btmm_process/flyfox.py
@@ -1,4 +1,6 @@
 import numpy as np
+import sys
+import xarray as xr
 
 
 def hypsometric_height(T_v, p_1, p_2):
@@ -81,3 +83,90 @@ def potential_temperature(T, p_0, p):
 
     theta = T * (p_0 / p) ** (R_cp)
     return theta
+
+
+def height_interp(section, z_bottom, num_layers=None, flip=False):
+    '''
+    Determine the linear height to interpolate along for each time step.
+    '''
+    # Number of points within each profile, this does not change with time.
+    if num_layers:
+        n_laf = num_layers
+    else:
+        n_laf = section.LAF.size
+
+    # Create the z coordinates for this time step using similar
+    # assumptions as above, but for just this time step.
+    if not flip:
+        z_temp = np.linspace(z_bottom, section['top'], num=n_laf)
+    else:
+        z_temp = np.linspace(section['top'], z_bottom, num=n_laf)
+    # Interpolate the profile to the average height spacing found above.
+    section.coords['z'] = ('z', z_temp)
+
+    return section
+
+
+def height_interp_time(section, z_bottom):
+    '''
+    Create a uniform height coordinate through time for a tethered balloon
+    with a time varying height.
+    '''
+    # Find just the section to be labeled.
+    LAF = section.LAF.values
+
+    # Determine which direction the fiber is oriented
+    if LAF[0] > LAF[1]:
+        flip = True
+    else:
+        flip = False
+
+    # --------
+    # Determine the linear height to interpolate along for each time step.
+
+    # Number of points within each profile, this does not change with time.
+    n_laf = section.LAF.size
+
+    # The average height of the profile. This is used for creating the
+    # height step for the linear interpolation.
+    z_top_avg = section['top'].mean(dim='time').values
+
+    # The maximum height of the layer. This sets the top value that the
+    # profile is interpolated to.
+    z_top_max = section['top'].max(dim='time').values
+
+    # The average height step for the profile.
+    delta_z_avg = (z_top_avg - z_bottom) / n_laf
+
+    # The interpolated height, using the average height step and the
+    # maximum height of the flight.
+    z_int = np.arange(z_bottom, z_top_max, delta_z_avg)
+
+    # For each time step interpolate the the observed profile to the
+    # heights derived above. The assumptions used mean that the
+    # interpolated points are closer together than the delta_LAF
+    # points. This is effectively downsampling.
+    ds_to_label = []
+    for n, t in enumerate(section.time):
+        sys.stdout.write('\r' + 'Time step ' + str(n + 1) + ' of '
+                         + str(np.size(section.time.values)))
+
+        # Select this time step.
+        temp = section.sel(time=t)
+
+        # Create the z coordinates for this time step using similar
+        # assumptions as above, but for just this time step.
+        if not flip:
+            z_temp = np.linspace(z_bottom, temp['top'], num=n_laf)
+        else:
+            z_temp = np.linspace(temp['top'], z_bottom, num=n_laf)
+        # Interpolate the profile to the average height spacing found above.
+        temp.coords['z'] = ('LAF', z_temp)
+        temp = temp.swap_dims({'LAF': 'z'}).interp(z=z_int)
+
+        # Append this time step to the container for the section
+        ds_to_label.append(temp)
+
+    # Concatenate along the time dimension
+    out = xr.concat(ds_to_label, 'time')
+    return out
