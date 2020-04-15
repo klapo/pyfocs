6f33e6666671d8abf9b3f0c82c2c872e2cfa5e07
Karl Lapo <lapo.karl@gmail.com>
2020-04-15 10:05:35 +0200

    Removing reference to deprecated functions.

--- src/pyfocs/__init__.py
+++ src/pyfocs/__init__.py
@@ -2,7 +2,6 @@ from .readDTS import archive_read, xml_read
 from .labeler import labelLoc_additional
 from .labeler import yamlDict, create_multiindex
 from .calibrate import matrixInversion
-from .calibrate import timeAvgCalibrate
 from .dtsarch import archiver
 from .dts_plots import bath_check, bath_validation, bias_violin
 from .stats import noisymoments, norm_xcorr
