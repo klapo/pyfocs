2a1dc3515133de4a18bc1e1cd2db9c93c6f0fc89
klapo <lapo.karl@gmail.com>
2021-05-03 14:39:31 +0200

    Added unique indexer function.

--- src/pyfocs/xr_helper.py
+++ src/pyfocs/xr_helper.py
@@ -10,3 +10,28 @@ def swap_sel(ds, coord1, coord2, selection):
         ret_ds = ds.swap_dims({coord1: coord2}).loc[{coord2: selection}]
 
     return(ret_ds)
+
+
+def xr_unique_index(ds, dim):
+    '''
+    It is not uncommon for DTS data to have shared xyz labels, e.g. at vertices
+    between line segments, creating non-unique indices. This problem makes it
+    impossible to use many xarray functions that rely on unique indices. It
+    can be fixed by re-indexing using just unique values. Nominally, this
+    approach throws away a point, but testing has revealed this to be
+    acceptable.
+
+    Parameters
+    ----------
+        ds : xarray dataset, contains dimension 'dim'
+        dim : string, corresponding to dimension in ds
+
+    Returns
+    ----------
+        ds : xarray with non-unique labels along 'dim' dropped
+    '''
+
+
+    _, index = np.unique(ds[dim], return_index=True)
+    ds = ds.isel({dim: index})
+    return ds
